1686abfd5708c9e1694237961b68fb76f23aab62
TajbiurShahriorRimon <tajbiurshahriorrimon@gmail.com>
2021-06-25 21:55:02 +0600

    5th

--- app/Models/org.php
+++ app/Models/org.php
@@ -7,13 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class org extends Model
 {
-<<<<<<< HEAD:app/Models/Users.php
-    protected $table = 'users';
-    protected $primaryKey = 'userId';
-    //use HasFactory;
-=======
     protected $table = 'org';
     protected $primaryKey = 'id';
     public $timestamps = false;
->>>>>>> main:app/Models/org.php
 }
