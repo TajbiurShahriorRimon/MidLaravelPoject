cf565724254584a86f09f5838129dd86fd7a8b13
ehsan-0801 <65059554+ehsan-0801@users.noreply.github.com>
2021-07-02 09:32:26 +0600

    front controller added

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,9 @@ use Illuminate\Support\Facades\Route;
 use \App\Http\Controllers\UserController;
 use \App\Http\Controllers\DonationController;
 use \App\Http\Controllers\OrganizerController;
-
+use \App\Http\Controllers\EventController;
+use \App\Http\Controllers\NotificationController;
+use \App\Http\Controllers\LogoutController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -19,20 +21,45 @@ use \App\Http\Controllers\OrganizerController;
 // Route::get('/', function () {
 //     return view('welcome');
 // });
-
-Route::view('/user/admin/index', 'user.admin.index');
-Route::get('/transaction', [\App\Http\Controllers\TransactionController::class, 'index']);
-Route::get('/userList', [UserController::class, 'index']);
-Route::get('/user/profile/{id}', [UserController::class, 'show']);
-Route::get('/user/changeStatus/{id}/{status}', [UserController::class, 'changeStatus']);
-Route::view('/addManager', 'manager.create');
-Route::post('/addManager', [UserController::class, 'addManager']);
-Route::get('/donationReport/yearly', [DonationController::class, 'index']);
-Route::get('/donorList', [DonationController::class, 'donorList']);
-Route::get('/topDonor', [DonationController::class, 'topDonor']);
-Route::get('/nonDonorList', [DonationController::class, 'nonDonorList']);
-Route::get('/nonOrganizerList', [OrganizerController::class, 'nonOrganizerList']);
-Route::get('/topOrganizer', [OrganizerController::class, 'topOrganizerDetails']);
+Route::group(['middleware'=>['sess']], function() {
+    Route::view('/user/admin/index', 'user.admin.index');
+    Route::get('/transactions', [\App\Http\Controllers\TransactionController::class, 'index']);
+    Route::get('/userList', [UserController::class, 'index']);
+    Route::get('/user/profile/{id}', [UserController::class, 'show']);
+    Route::get('/user/changeStatus/{id}/{status}', [UserController::class, 'changeStatus']);
+    Route::view('/addManager', 'manager.create');
+    Route::post('/addManager', [UserController::class, 'addManager']);
+    Route::get('/donationReport/yearly', [DonationController::class, 'index']);
+    Route::get('/donationReport/monthly/{year}', [DonationController::class, 'monthlyDonationReport']);
+    Route::get('/donorList', [DonationController::class, 'donorList']);
+    Route::get('/topDonor', [DonationController::class, 'topDonor']);
+    Route::get('/nonDonorList', [DonationController::class, 'nonDonorList']);
+    Route::get('/nonOrganizerList', [OrganizerController::class, 'nonOrganizerList']);
+    Route::get('/topOrganizer', [OrganizerController::class, 'topOrganizerDetails']);
+    Route::get('/userHomePage/events', [EventController::class, 'index']); //Home page for admin
+    Route::post('/userHomePage/events', [EventController::class, 'searchActiveEvents']);
+    Route::get('/events/eventRequest', [EventController::class, 'eventRequest']);
+    Route::get('/eventRequest/approveForm/{id}', [EventController::class, 'approveForm']);
+    Route::post('/eventRequest/approveForm/{id}', [EventController::class, 'confirmCreateEvent']);
+    Route::get('/organizerList/report', [OrganizerController::class, 'organizerNumOfEvents']);
+    Route::get('/organizerReport/yearly/{id}', [OrganizerController::class, 'organizerYearEventReport']);
+    Route::get('/event/smallReport/{id}', [EventController::class, 'eventBriefDetails']);
+    Route::get('/event/removeActiveEvent/{id}', [EventController::class, 'showEventForRemove']);
+    Route::get('/event/confirmRemoveEvent/{id}', [EventController::class, 'removeActiveEvent']);
+    Route::get('/admin/notice', [NotificationController::class, 'adminNotification']);
+    Route::get('/admin/readNotice/{id}', [NotificationController::class, 'adminReadNotice']);
+    Route::get('/admin/logout', [LogoutController::class, 'index']);
+    Route::get('/events/removePendingEvent/{id}', [EventController::class, 'removePendingEvent']);
+    Route::get('/events/archivedEvents', [EventController::class, 'archivedEvents']);
+    Route::get('/event/information/{id}', [EventController::class, 'eventInformation']);
+    Route::post('/event/information/{id}', [EventController::class, 'changeManagerForEvent']);
+    Route::get('/event/chooseManagerForUpdate/{id}', [EventController::class, 'chooseManagerForEventUpdate']);
+    Route::post('/event/chooseManagerForUpdate/{id}', [EventController::class, 'changeManagerForEvent']);
+    Route::get('/admin/createNotice', [NotificationController::class, 'adminCreateNotice']);
+    Route::post('/admin/createNotice', [NotificationController::class, 'adminSendNotice']);
+    Route::get('/event/detailReviews/{id}', [EventController::class, 'detailReviews']);
+    Route::get('/event/removeComment/{id}/{eventId}', [EventController::class, 'removeEventComment']);
+});
 
 
 Route::get('/login', 'loginController@index');
@@ -54,12 +81,19 @@ Route::group(['middleware'=>['sess']], function(){
     Route::post('/create', 'orgCreateController@add');
 
     Route::get('/org_dashboard', 'orgDashboardController@index');
+    Route::post('/org_dashboard', 'orgDashboardController@search');
 
     Route::get('/org/mycampaign', 'orgMycampaign@index');
     Route::get('/org/delete/{eId}', 'orgMycampaign@delete');
+    Route::get('/org/edit/{eId}', 'orgEditCampaign@index');
+    Route::post('/org/edit/{eId}', 'orgEditCampaign@update');
 
-    Route::get('/campaignDetails', 'orgCampaignDetails@index');
-    Route::get('/transaction', 'orgCampaignTran@index');
+
+    Route::get('/campaignDetails/{eId}', 'orgCampaignDetails@index');
+    Route::post('/campaignComment/{eId}', 'orgCampaignComment@insert');
+
+    Route::get('/donate/transaction/{eId}', 'orgCampaignTran@index');
+    Route::post('/donate/transaction/{eId}', 'orgCampaignTran@sendMoney');
 
     Route::get('/details', 'orgDetails@index')->name('org.details');
     Route::get('/editProfile', 'orgEditProfile@index');
